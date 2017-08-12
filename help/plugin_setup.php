1313adf17504c9a77d701db83065b358883fe61f
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-08-12 06:29:44 -0600

    Adding help

--- help/plugin_setup.php
+++ help/plugin_setup.php
@@ -5,3 +5,33 @@
 <p>
 Welcome to the Projector Control plugin
 
+<table border="1">
+<tr>
+<td>
+Projector Make
+</td>
+<td>
+Model
+</td>
+<td>
+Cable
+</td>
+<td>
+Link to Manual (if available>
+</td>
+</tr>
+
+<tr>
+<td>
+Hitatchi
+</td>
+<td>
+CPX 1250
+</td>
+<td>
+USB / Serial
+</td>
+</tr>
+
+</table>
+
