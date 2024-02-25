749c7fe928c2a7c059a517284c1e8b6f3f4c534d
Rick Harris <poporacer@yahoo.com>
2024-02-24 19:47:27 -0700

    Fix Close button on upgade complete

--- commonFunctions.inc.php
+++ commonFunctions.inc.php
@@ -6,5 +6,9 @@ $logFile = $settings['logDirectory']."/".$pluginName.".log";
 $pluginName = basename(dirname(__FILE__));  //pjd 7-10-2019   added per dkulp
 $pluginUpdateFile = $settings['pluginDirectory']."/".$pluginName."/"."pluginUpdate.inc";
 
-
+function PluginProgressDialogDone() {
+    $('#pluginsProgressPopupCloseButton').prop("disabled", false);
+    DisplayProgressDialog("pluginsProgressPopup", "Upgrade Plugin");
+    StreamURL(url, 'pluginsProgressPopupText', 'PluginProgressDialogDone', 'PluginProgressDialogDone');
+}
 ?>
